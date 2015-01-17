f001eeb6dcad6712c037198afc491a0bb8e0acbe
Rafa <raphael.w@hotmail.com>
2015-01-17 18:43:12 +0100

    I killed your change!! Muaaahaaaaa

--- Thesis Template Rafa/_Make_index.cmd
+++ Thesis Template Rafa/_Make_index.cmd
@@ -2,6 +2,6 @@ set document=Rafa Template
 set ist_path=C:\Program Files\Latex\Miktex\makeindex\nomencl\nomencl.ist
 
 makeindex "%document%.nlo" -s "%ist_path%" -o "%document%.nls"
-rem "das ist eine Änderung"
+
 
 pause
