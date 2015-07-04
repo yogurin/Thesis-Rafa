70ed7dc51fe81c2c9b96a5a83534f6d1fcf2dfea
Rafa <raphael.w@hotmail.com>
2015-07-04 19:28:15 +0200

    Manuscript Intro and Results

--- Thesis Template Rafa/_Make_index.cmd
+++ Thesis Template Rafa/_Make_index.cmd
@@ -1,6 +1,5 @@
 set document=Rafa Template
-set ist_path=C:\Program Files\Latex\Miktex\makeindex\nomencl\nomencl.ist
-
+set ist_path=C:\Program Files\MiKTeX 2.9\makeindex\nomencl\nomencl.ist
 makeindex "%document%.nlo" -s "%ist_path%" -o "%document%.nls"
 
 
